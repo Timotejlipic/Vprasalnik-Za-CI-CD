019c984a2333dcb8ded4fdbd2457a58838d34cbd
utro007 <aljaz.utrosa19@gmail.com>
2026-05-15 15:53:02 +0200

    Dodan gost
    Dodan pogled kot gost in admin

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,15 @@ import React, { useState } from 'react';
 import { questionnaireCategories, maturityRules, mockPipelines } from './data.js';
 import Sidebar from './components/Sidebar.jsx';
 import Header from './components/Header.jsx';
-import LoginOverlay from './components/LoginOverlay.jsx';
+import LandingPage from './components/LandingPage.jsx';
 import Dashboard from './components/Dashboard.jsx';
-import Assessment1 from './components/Assessment1.jsx';
-import Assessment2 from './components/Assessment2.jsx';
-import Assessment3 from './components/Assessment3.jsx';
-import Assessment4 from './components/Assessment4.jsx';
+import AssessmentWrapper from './components/AssessmentWrapper.jsx';
 import Builder from './components/Builder.jsx';
 import Rules from './components/Rules.jsx';
 
 export default function App() {
+  const [appState, setAppState] = useState('landing');
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [user, setUser] = useState(null);
   const [currentView, setCurrentView] = useState('dashboard');
@@ -48,13 +47,7 @@ export default function App() {
       case 'dashboard':
         return <Dashboard pipelines={pipelines} setPipelines={setPipelines} switchView={switchView} loadAssessment={loadAssessment} resetAssessment={resetAssessment} />;
       case 'assessment':
-        return <Assessment1 isLoggedIn={isLoggedIn} pipelines={pipelines} setPipelines={setPipelines} currentAssessment={currentAssessment} setCurrentAssessment={setCurrentAssessment} currentAssessmentId={currentAssessmentId} categories={categories} rules={rules} switchView={switchView} />;
-      case 'assessment2':
-        return <Assessment2 isLoggedIn={isLoggedIn} pipelines={pipelines} setPipelines={setPipelines} currentAssessment={currentAssessment} setCurrentAssessment={setCurrentAssessment} currentAssessmentId={currentAssessmentId} categories={categories} rules={rules} switchView={switchView} />;
-      case 'assessment3':
-        return <Assessment3 isLoggedIn={isLoggedIn} pipelines={pipelines} setPipelines={setPipelines} currentAssessment={currentAssessment} setCurrentAssessment={setCurrentAssessment} currentAssessmentId={currentAssessmentId} categories={categories} rules={rules} switchView={switchView} />;
-      case 'assessment4':
-        return <Assessment4 isLoggedIn={isLoggedIn} pipelines={pipelines} setPipelines={setPipelines} currentAssessment={currentAssessment} setCurrentAssessment={setCurrentAssessment} currentAssessmentId={currentAssessmentId} categories={categories} rules={rules} switchView={switchView} />;
+        return <AssessmentWrapper isLoggedIn={isLoggedIn} pipelines={pipelines} setPipelines={setPipelines} currentAssessment={currentAssessment} setCurrentAssessment={setCurrentAssessment} currentAssessmentId={currentAssessmentId} categories={categories} rules={rules} switchView={switchView} />;
       case 'builder':
         return <Builder categories={categories} setCategories={setCategories} />;
       case 'rules':
@@ -64,19 +57,31 @@ export default function App() {
     }
   };
 
-  return (
-    <>
-      <LoginOverlay isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} setUser={setUser} />
+  const enterAsGuest = () => {
+    setAppState('app');
+  };
 
-      <Sidebar currentView={currentView} switchView={switchView} />
+  const enterAsAdmin = (username) => {
+    setUser(username);
+    setIsLoggedIn(true);
+    setAppState('app');
+  };
+
+  if (appState === 'landing') {
+    return <LandingPage enterAsGuest={enterAsGuest} enterAsAdmin={enterAsAdmin} />;
+  }
+
+  return (
+    <div id="app">
+      <Sidebar currentView={currentView} switchView={switchView} isLoggedIn={isLoggedIn} isOpen={isSidebarOpen} />
 
       <div className="main-content">
-        <Header isLoggedIn={isLoggedIn} user={user} setIsLoggedIn={setIsLoggedIn} setUser={setUser} />
+        <Header isLoggedIn={isLoggedIn} user={user} setIsLoggedIn={setIsLoggedIn} setUser={setUser} setAppState={setAppState} toggleSidebar={() => setIsSidebarOpen(!isSidebarOpen)} isSidebarOpen={isSidebarOpen} />
 
         <div className="page-view active">
           {renderView()}
         </div>
       </div>
-    </>
+    </div>
   );
 }
